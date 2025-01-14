e0ebdb11e019247bcea0b40feeee61e51d7ac0ec
danibahnani <danielpadban@gmail.com>
2025-01-14 11:55:56 +0100

    cleaned up repo

--- data_zip/open_data_zip.py
+++
@@ -1,5 +0,0 @@
-import zipfile
-
-with zipfile.ZipFile('EMNIST_zip.zip','r') as zip_file:
-    zip_file.printdir()
-    zip_file.extractall('EMNIST_data')
--- save_models/tfconvert.py
+++
@@ -1,7 +0,0 @@
-import onnx
-from onnx_tf.backend import prepare
-
-onnx_model = onnx.load('save_models/onnx_models/2024-09-30 18_13_15.758843.onnx')
-tf_rep = prepare(onnx_model)
-tf_rep.export_graph('save_models/tf_models/2024-09-30 18_13_15.758843.pb')
-
